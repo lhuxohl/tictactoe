77043e42e980c775b8b4deddb62ab3c998c806fb
lhuxohl <zeephyros@gmail.com>
2022-08-26 15:14:49 +0200

    replaced board with easier array representation

--- tictactoe.py
+++ tictactoe.py
@@ -1,221 +1,317 @@
 #!/usr/bin/python3
 
-# initializing global variables
-player = 'O'
-game = [
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ''],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '']
-]  # the board consists of a tictcatoe board in every field which contains an extra space for winning conditions
+# initializing gloabl variables
+player = 'X'
+game = [[[[' ' for i in range(3)] for j in range(3)]
+         for k in range(3)] for l in range(3)]
+
+top = 0
+left = 0
+mid = 1
+right = 2
+bot = 2
 
 
-# method to print the complete game
 def print_game():
-    for i in range(0, len(game), 3):
-        print(
-            game[i][0] + '|' + game[i][1] + '|' + game[i][2] + ' ║ '
-            + game[i + 1][0] + '|' + game[i + 1][1] + '|' + game[i + 1][2] + ' ║ '
-            + game[i + 2][0] + '|' + game[i + 2][1] + '|' + game[i + 2][2]
-        )
-        print(
-            game[i][3] + '|' + game[i][4] + '|' + game[i][5] + ' ║ '
-            + game[i + 1][3] + '|' + game[i + 1][4] + '|' + game[i + 1][5] + ' ║ '
-            + game[i + 2][3] + '|' + game[i + 2][4] + '|' + game[i + 2][5]
-        )
-        print(
-            game[i][6] + '|' + game[i][7] + '|' + game[i][8] + ' ║ '
-            + game[i + 1][6] + '|' + game[i + 1][7] + '|' + game[i + 1][8] + ' ║ '
-            + game[i + 2][6] + '|' + game[i + 2][7] + '|' + game[i + 2][8]
-        )
-        if i == 0 or i == 3:
-            print('══════╬═══════╬══════')
+    '''
+    Method for printing the current game
+    '''
+    for i in range(3):
+        for j in range(3):
+            str = ''
 
-    print()
+            for k in range(3):
+                for l in range(3):
+                    str += game[i][k][j][l]
 
+                    if l != 2:
+                        str += '|'
+                if k != 2:
+                    str += u' \u2551 '
 
-# method to print the selected board
-def print_board(board):
-    print(game[board][0] + '|' + game[board][1] + '|' + game[board][2])
-    print(game[board][3] + '|' + game[board][4] + '|' + game[board][5])
-    print(game[board][6] + '|' + game[board][7] + '|' + game[board][8])
+            print(str)
+        if i != 2:
+            print(u'\u2550\u2550\u2550\u2550\u2550\u2550\u256c\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u256c\u2550\u2550\u2550\u2550\u2550\u2550')
 
 
-def draw_game():
-    for i in range(len(game)):
-        if game[i][9] == '':
-            return False
-    return True
+def print_board(cords):
+    '''
+    Method for printing the selected board
+    :param board_x - int - x-coordinate of board
+    :param board_y - int - y-coordinate of board
+    '''
+    board_x = cords[0]
+    board_y = cords[1]
+    for j in range(3):
+        str = ''
+        for l in range(3):
+            str += game[board_x][board_y][j][l]
+
+            if l != 2:
+                str += '|'
+        print(str)
 
 
-# checks if the game is win by the current player
-def won_game():
+def convert_coords_to_input(coords):
+    '''
+    Method that converts coord back to input value
+    :param coords - tuple - coords
+    :return - int - input value
+    '''
+    switcher = {
+        (2, 0): 1,
+        (2, 1): 2,
+        (2, 2): 3,
+        (1, 0): 4,
+        (1, 1): 5,
+        (1, 2): 6,
+        (0, 0): 7,
+        (0, 1): 8,
+        (0, 2): 9
+    }
+
+    return switcher.get(coords)
+
+
+def convert_input_to_coords(input):
+    '''
+    Method that converts input value to board coordinates
+    :param input - int - number input
+    :returns - tuple - x- and y-coordinate of board
+    '''
+    switcher = {
+        1: (2, 0),
+        2: (2, 1),
+        3: (2, 2),
+        4: (1, 0),
+        5: (1, 1),
+        6: (1, 2),
+        7: (0, 0),
+        8: (0, 1),
+        9: (0, 2)
+    }
+
+    if (not (input in switcher.keys())):
+        return -1
+    return switcher.get(input)
+
+
+def board_won(board):
+    '''
+    Checks if the given board is won by any player
+    :param board - [[String]] - current board
+    :return - bool - board won
+    '''
+    if board_won_by_player(board, 'X') or board_won_by_player(board, 'O'):
+        return True
+
+    return False
+
+
+def board_won_by_player(board, player):
+    '''
+    Checks if the given board is won by any player
+    :param board - [[String]] - current board
+    :param player - String - selected player
+    :return - bool - board won by player
+    '''
     if (
-            (game[0][9] == player and game[1][9] == player and game[2][9] == player) or  # horizontal top
-            (game[3][9] == player and game[4][9] == player and game[5][9] == player) or  # horizontal middle
-            (game[6][9] == player and game[7][9] == player and game[8][9] == player) or  # horizontal bottom
-            (game[0][9] == player and game[4][9] == player and game[8][9] == player) or  # diagonal topleft-bottomright
-            (game[2][9] == player and game[4][9] == player and game[6][9] == player) or  # diagonal topright-bottomleft
-            (game[0][9] == player and game[3][9] == player and game[6][9] == player) or  # vertical left
-            (game[1][9] == player and game[4][9] == player and game[7][9] == player) or  # vertical middle
-            (game[2][9] == player and game[5][9] == player and game[8][9] == player)     # vertical bottom
+        board[top][left] == player and board[top][mid] == player and board[top][right] == player or
+        board[mid][left] == player and board[mid][mid] == player and board[mid][right] == player or
+        board[bot][left] == player and board[bot][mid] == player and board[bot][right] == player or
+        board[top][left] == player and board[mid][left] == player and board[bot][left] == player or
+        board[top][mid] == player and board[mid][mid] == player and board[bot][mid] == player or
+        board[top][right] == player and board[mid][right] == player and board[bot][right] == player or
+        board[top][left] == player and board[mid][mid] == player and board[bot][right] == player or
+        board[bot][left] == player and board[mid][mid] == player and board[top][right] == player
     ):
         return True
+
     return False
 
 
-# checks if thr game ended in a draw of if a player won
-def done():
-    if won_game():
+def board_draw(board):
+    '''
+    Checks if a board ended in a draw
+    :param board - [[String]] - current board
+    :return - bool - board ended in a draw
+    '''
+    if not board_won(board):
+        for i in range(3):
+            for k in range(3):
+                if board[i][k] == ' ':
+                    return False
         return True
     return False
 
 
-# changes the number selected to the correct position of the array
-def get_number(num):
-    switcher = {
-        1: 6,
-        2: 7,
-        3: 8,
-        4: 3,
-        5: 4,
-        6: 5,
-        7: 0,
-        8: 1,
-        9: 2,
-    }
-    if (not (num in switcher.keys())):
-        return -1
-    return switcher.get(num)
+def is_valid_board(cords):
+    '''
+    Checks if a selected board is a valid selection
+    :return - bool - selected board valid
+    '''
 
+    if cords == -1:
+        return False
 
-# checks if the board can be selected
-def valid_board(board):
-    if board < 0 or board > 8:
+    board_x = cords[0]
+    board_y = cords[1]
+    if board_won(game[board_x][board_y]) or board_draw(game[board_x][board_y]):
         return False
-    if not game[board][9] == '':
-        return False
+
     return True
 
 
-# selects the board that will be used for the move
-def select_board():
-    print('Please select a board:')
-
-    temp = input()
-    if temp == '':
-        exit()
-    temp = int(temp)
-    temp = get_number(temp)
-
-    while not valid_board(temp):
-        print('Not a valid board. Please try again:')
-
-        temp = input()
-        if temp == '':
-            exit()
-        temp = int(temp)
-
-        temp = get_number(temp);
-
-    return temp
-
-
-# checks if the selected field is valid
-def valid_field(board, field):
-    if game[board][field] == ' ':
+def game_won():
+    '''
+    Checks if the game was won by either player
+    '''
+    if (
+        board_won(game[top][left]) and board_won(game[top][mid]) and board_won(game[top][right]) or
+        board_won(game[mid][left]) and board_won(game[mid][mid]) and board_won(game[mid][right]) or
+        board_won(game[bot][left]) and board_won(game[bot][mid]) and board_won(game[bot][right]) or
+        board_won(game[top][left]) and board_won(game[mid][left]) and board_won(game[bot][left]) or
+        board_won(game[top][mid]) and board_won(game[mid][mid]) and board_won(game[bot][mid]) or
+        board_won(game[top][right]) and board_won(game[mid][right]) and board_won(game[bot][right]) or
+        board_won(game[top][left]) and board_won(game[mid][mid]) and board_won(game[bot][right]) or
+        board_won(game[top][right]) and board_won(
+            game[mid][mid]) and board_won(game[bot][left])
+    ):
         return True
+
     return False
 
 
-# selects a valid field
-def select_field(board):
+def game_draw():
+    '''
+    Checks if the game is a drawn
+    '''
+    for i in range(1, 10):
+        if is_valid_board(convert_input_to_coords(i)):
+            return False
+
+    return True
+
+
+def is_valid_field(board_cords, field_cords):
+    '''
+    Checks if the selected field is valid
+    :param board_cords - tuple - board x- and y-coordinate
+    :param field_cords -- tuple - field x- and y-coodinate
+    :return - bool - valid field
+    '''
+
+    if field_cords == -1 or board_cords == -1:
+        return False
+
+    board_x = board_cords[0]
+    board_y = board_cords[1]
+    field_x = field_cords[0]
+    field_y = field_cords[1]
+
+    if game[board_x][board_y][field_x][field_y] == ' ':
+        return True
+
+    return False
+
+
+def select_field(baord_cords):
+    '''
+    Method to select a valid field on the currently selected board
+    :param board_cords - tuple - board x- and y-coordinate
+    :return - tuple - valid field cords
+    '''
     print('Please select a field:')
 
     temp = input()
-    if temp == '':
-        sys.exit()
-    temp = int(temp)
-    temp = get_number(temp)
+    if temp == '' or temp == '\n':
+        exit()
 
-    while not valid_field(board, temp):
-        print('Not a valid field. Please try again:')
+    temp = int(temp)
+    temp = convert_input_to_coords(temp)
+
+    while not is_valid_field(baord_cords, temp):
+        print('Not a valid field! Please try again:')
 
         temp = input()
-        if temp == '':
-            sys.exit()
-        temp = int(temp)
 
-        temp = get_number(temp)
+        if temp == '' or temp == '\n':
+            exit()
+
+        temp = int(temp)
+        temp = convert_input_to_coords(temp)
 
     return temp
 
 
-# check if a board got won by a player
-def won_board(board):
-    if (
-            (game[board][0] == player and game[board][1] == player and game[board][2] == player) or  # horizontal top
-            (game[board][3] == player and game[board][4] == player and game[board][5] == player) or  # horizontal middle
-            (game[board][6] == player and game[board][7] == player and game[board][8] == player) or  # horizontal bottom
-            (game[board][0] == player and game[board][4] == player and game[board][8] == player) or  # diagonal topleft-bottomright
-            (game[board][2] == player and game[board][4] == player and game[board][6] == player) or  # diagonal topright-bottomleft
-            (game[board][0] == player and game[board][3] == player and game[board][6] == player) or  # vertical left
-            (game[board][1] == player and game[board][4] == player and game[board][7] == player) or  # vertical middle
-            (game[board][2] == player and game[board][5] == player and game[board][8] == player)     # vertical right
-    ):
-        for i in range(len(game[board])):
-            game[board][i] = player
-        return True
-    return False
+def select_board():
+    '''
+    Method to select a board.
+    '''
+    print('Please select a board:')
+
+    temp = input()
+    if temp == '' or temp == '\n':
+        exit()
+
+    temp = int(temp)
+    temp = convert_input_to_coords(temp)
+
+    while not is_valid_board(temp):
+        print('Not a valid board! Please try again:')
+
+        temp = input()
+
+        if temp == '' or temp == '\n':
+            exit()
+
+        temp = int(temp)
+        temp = convert_input_to_coords(temp)
+
+    return temp
 
 
-# check if a board ended in a draw
-def draw_board(board):
-    for i in range(len(game[board]) - 1):
-        if game[board][i] == ' ':
-            return False
+def game_over():
+    '''
+    Checks if the game should end.
+    :return - bool - game drawn or won by either player
+    '''
 
-    for i in range(len(game[board])):
-        game[board][i] = 'D'
-    return True
+    return game_won() or game_draw()
 
 
-if __name__ == '__main__':
-    selected_board = -1
+if __name__ == "__main__":
+    board_cords = -1
 
-    while not done():
+    while not game_over():
         if player == 'X':
             player = 'O'
         else:
             player = 'X'
-
+        print(f'It is player {player}\'s turn.')
         print_game()
 
-        if not valid_board(selected_board):
-            selected_board = select_board()
+        if not is_valid_board(board_cords):
+            board_cords = select_board()
 
-        print('It is ', player, '\'s turn.')
-        print(selected_board, 'selected.')
+        print(f'Board {convert_coords_to_input(board_cords)} selected.')
+        print_board(board_cords)
 
-        print_board(selected_board)
+        field_cords = select_field(board_cords)
+        game[board_cords[0]][board_cords[1]
+                             ][field_cords[0]][field_cords[1]] = player
 
-        selected_field = select_field(selected_board)
+        if board_won(game[board_cords[0]][board_cords[1]]):
+            print(
+                f'Player {player} won board {convert_coords_to_input(board_cords)}!')
 
-        game[selected_board][selected_field] = player
+        elif board_draw(game[board_cords[0]][board_cords[1]]):
+            print(
+                f'Board {convert_coords_to_input(board_cords)} ended in a draw!')
 
-        if won_board(selected_board):
-            print('Player', player, 'won board', selected_board, '!')
-        elif draw_board(selected_board):
-            print('Board', selected_board, 'ended in a draw!')
+        board_cords = field_cords
 
-        selected_board = selected_field
-
-    if won_game():
-        print(player, 'has won the game!')
+    if game_won():
+        print(f'Player {player} won the game!')
     else:
         print('The game ended in a draw!')
